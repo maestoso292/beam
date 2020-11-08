8946954a6c7ea49ec28da3212bc1e3f87adb8177
maestoso292 <65017095+maestoso292@users.noreply.github.com>
2020-11-08 18:42:46 +0800

    Refactored to only use one AnimatorSet. Views and Animators stored in HashMap

--- app/src/main/java/com/example/beam/MainActivity.java
+++ app/src/main/java/com/example/beam/MainActivity.java
@@ -1,106 +1,45 @@
 package com.example.beam;
 
+import android.animation.Animator;
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView blackbg;
-    ImageView overlay;
-    ImageView bar1;
-    ImageView bar2;
-    ImageView bar3;
-    ImageView bar4;
-    ImageView bar5;
-    ImageView bar6;
-    TextView beamtxt;
-    ConstraintLayout bars;
-
-
+    HashMap<View, Animator> animHashMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        beamtxt = (TextView) findViewById(R.id.beamTxt);
-        blackbg = (ImageView) findViewById(R.id.blackBg);
-        overlay = (ImageView) findViewById(R.id.whiteOverlay);
-        bar1 = (ImageView) findViewById(R.id.bar_1);
-        bar2 = (ImageView) findViewById(R.id.bar_2);
-        bar3 = (ImageView) findViewById(R.id.bar_3);
-        bar4 = (ImageView) findViewById(R.id.bar_4);
-        bar5 = (ImageView) findViewById(R.id.bar_5);
-        bar6 = (ImageView) findViewById(R.id.bar_6);
-        bars = (ConstraintLayout) findViewById (R.id.bars);
+        animHashMap = new HashMap<>();
+        addAnimToHashMap(R.id.beamTxt, R.animator.firstanim);
+        addAnimToHashMap(R.id.blackBg, R.animator.bganim);
+        addAnimToHashMap(R.id.whiteOverlay, R.animator.overlayanim);
+        addAnimToHashMap(R.id.bars, R.animator.barsappear);
+        addAnimToHashMap(R.id.bar_1, R.animator.bar_1);
+        addAnimToHashMap(R.id.bar_2, R.animator.bar_2);
+        addAnimToHashMap(R.id.bar_3, R.animator.bar_3);
+        addAnimToHashMap(R.id.bar_4, R.animator.bar_4);
+        addAnimToHashMap(R.id.bar_5, R.animator.bar_5);
+        addAnimToHashMap(R.id.bar_6, R.animator.bar_6);
 
+        for (Map.Entry<View, Animator> entry : animHashMap.entrySet()) {
+            entry.getValue().setTarget(entry.getKey());
+        }
 
-        AnimatorSet blinktxt = (AnimatorSet) AnimatorInflater.loadAnimator(beamtxt.getContext(),
-                R.animator.firstanim);
-        blinktxt.setTarget(beamtxt);
-        blinktxt.start();
-
-        AnimatorSet bgAnim = (AnimatorSet) AnimatorInflater.loadAnimator(blackbg.getContext(),
-                R.animator.bganim);
-        bgAnim.setTarget(blackbg);
-        bgAnim.start();
-
-/*//      View animation tryout (might not need it anymore)
-        Animation shrinkdown = AnimationUtils.loadAnimation(this, R.anim.shrinkdown);
-        blackbg.startAnimation(shrinkdown);*/
-
-        AnimatorSet overlayAnim = (AnimatorSet) AnimatorInflater.loadAnimator(overlay.getContext(),
-                R.animator.overlayanim);
-        overlayAnim.setTarget(overlay);
-        overlayAnim.start();
-
-        AnimatorSet barsAppear = (AnimatorSet) AnimatorInflater.loadAnimator(bars.getContext(),
-                R.animator.barsappear);
-        barsAppear.setTarget(bars);
-        barsAppear.start();
-
-        AnimatorSet bar1scale = (AnimatorSet) AnimatorInflater.loadAnimator(bar1.getContext(),
-                R.animator.bar_1);
-        bar1scale.setTarget(bar1);
-        bar1scale.start();
-
-        AnimatorSet bar2scale = (AnimatorSet) AnimatorInflater.loadAnimator(bar2.getContext(),
-                R.animator.bar_2);
-        bar2scale.setTarget(bar2);
-        bar2scale.start();
-
-        AnimatorSet bar3scale = (AnimatorSet) AnimatorInflater.loadAnimator(bar3.getContext(),
-                R.animator.bar_3);
-        bar3scale.setTarget(bar3);
-        bar3scale.start();
-
-        AnimatorSet bar4scale = (AnimatorSet) AnimatorInflater.loadAnimator(bar4.getContext(),
-                R.animator.bar_4);
-        bar4scale.setTarget(bar4);
-        bar4scale.start();
-
-        AnimatorSet bar5scale = (AnimatorSet) AnimatorInflater.loadAnimator(bar5.getContext(),
-                R.animator.bar_5);
-        bar5scale.setTarget(bar5);
-        bar5scale.start();
-
-        AnimatorSet bar6scale = (AnimatorSet) AnimatorInflater.loadAnimator(bar6.getContext(),
-                R.animator.bar_6);
-        bar6scale.setTarget(bar6);
-        bar6scale.start();
-
-
-
-
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(animHashMap.values());
+        animatorSet.start();
     }
 
-
+    private void addAnimToHashMap(int viewId, int animatorId) {
+        animHashMap.put(findViewById(viewId), AnimatorInflater.loadAnimator(this, animatorId));
+    }
 }
